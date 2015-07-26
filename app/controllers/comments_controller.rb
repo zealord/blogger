a94fa0b3a5923a07155043d542fe7c368a564dc1
Thanh Le <letienthanh@gmail.com>
2015-07-26 15:37:26 +0700

    finished blog comments feature

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+  include CommentsHelper
+  def create
+    #fail
+    @comment = Comment.new( comment_params )
+    @comment.article_id = params[:article_id]
+
+    @comment.save
+
+    redirect_to article_path(@comment.article)
+    # why is it the same to 
+    # redirect_to article_path(@comment.article_id)
+  end
+end
